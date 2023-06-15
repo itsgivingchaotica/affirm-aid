4260fbcf7723c2479eb44bdd58e6cd18aceb3ee3
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-06-15 14:51:53 -0400

    fix paths

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,10 +1,10 @@
 $(function () {
-    $("#navbar-placeholder").load("../components/navbar.html", function () {
+    $("#navbar-placeholder").load("./components/navbar.html", function () {
         // Update the href values based on the current page
         var currentPagePath = window.location.pathname;
 
-        $(".logo").attr("href", "../index.html"); 
-        $(".about-btn").attr("href", "about.html"); 
-        $(".donate-btn").attr("href", "donate.html"); 
+        $(".logo").attr("href", "index.html"); 
+        $(".about-btn").attr("href", "pages/about.html"); 
+        $(".donate-btn").attr("href", "pages/donate.html"); 
     });
 });
